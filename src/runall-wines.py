4b91afb0a6b5656f2bb02a057ff23369b4aef96d
Sean Doyle <sdoyle@partners.org>
2018-05-09 12:09:21 -0400

    Add files to CI

--- src/runall-wines.py
+++ src/runall-wines.py
@@ -1,4 +1,6 @@
 import importlib
+
+import recipy
 subset = importlib.import_module('.data.01_subset-data-GBP', 'src')
 plotwines = importlib.import_module('.visualization.02_visualize-wines', 'src')
 country_sub = importlib.import_module('.data.03_country-subset', 'src')
